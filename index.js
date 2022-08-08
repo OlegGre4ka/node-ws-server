05a0881c1e4c752246370ba82aa9b5385faf232d
OlegGre4ka <gre4kae@ukr.net>
2022-08-08 19:27:44 +0300

    created ws server

---
+++ index.js
@@ -0,0 +1,28 @@
+const ws = require('ws');
+
+const wss = new ws.Server({
+    port: process.env.PORT || 5000
+}, () => console.log(`Server started on 5000`))
+
+console.log(wss,`wss`)
+wss.on('connection', function connection(ws) {
+    ws.on('message', function (message) {
+        message = JSON.parse(message)
+        switch (message.event) {
+            case 'message':
+                broadcastMessage(message)
+                break;
+            case 'connection':
+                broadcastMessage(message)
+                break;
+            default:
+                break;
+        }
+    })
+})
+
+function broadcastMessage(message, id) {
+    wss.clients.forEach(client => {
+        client.send(JSON.stringify(message))
+    })
+}
